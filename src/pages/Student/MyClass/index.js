e955a2b24dbf4f3fcc63660e55c13080be44fd85
Trong Nghia <nghia.nt2704@gmail.com>
2023-05-05 13:45:18 +0700

    Complete my class

--- src/pages/Student/MyClass/index.js
+++ src/pages/Student/MyClass/index.js
@@ -1,12 +1,57 @@
 import classNames from 'classnames/bind';
 import styles from './MyClass.module.scss';
+import { Link } from 'react-router-dom';
 
 const cx = classNames.bind(styles);
 
+const classList = [
+    {
+        image: 'imageClass.svg',
+        nameClass: 'Learn Chinese in 3 months for HSK3',
+        teacher: 'Tam Truong',
+    },
+    {
+        image: 'imageClass.svg',
+        nameClass: 'Learn Chinese in 3 months for HSK3',
+        teacher: 'Tam Truong',
+    },
+    {
+        image: 'imageClass.svg',
+        nameClass: 'Learn Chinese in 3 months for HSK3',
+        teacher: 'Tam Truong',
+    },
+    {
+        image: 'imageClass.svg',
+        nameClass: 'Learn Chinese in 3 months for HSK3',
+        teacher: 'Tam Truong',
+    },
+];
+
 function MyClass() {
+    const srcImage = process.env.PUBLIC_URL + '/svg/';
+
     return (
         <div className={cx('container')}>
-            <h1>MyClass</h1>
+            <h1>My Class</h1>
+            <div className={cx('wrap_class')}>
+                {classList.map((e, index) => {
+                    return (
+                        <Link key={index} className={cx('card')}>
+                            <img
+                                className={cx('imageClass')}
+                                src={srcImage + e.image}
+                                alt=""
+                            />
+                            <div className={cx('wrap_text')}>
+                                <h3 className={cx('nameClass')}>
+                                    {e.nameClass}
+                                </h3>
+                                <p className={cx('teacher')}>Teaching by {e.teacher}</p>
+                            </div>
+                        </Link>
+                    );
+                })}
+            </div>
         </div>
     );
 }
